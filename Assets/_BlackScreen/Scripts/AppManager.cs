10a1baef1bc265225210f2dda5ca690d8284a02a
Sota Funabashi <sfunabashi276@gmail.com>
2020-03-12 01:33:05 +0900

    add project

---
+++ Assets/_BlackScreen/Scripts/AppManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackScreen
+{
+    public class AppManager : MonoBehaviour
+    {
+        private static void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+#endif
+        }
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Quit();
+            }
+        }
+    }
+}
---
+++ Assets/_BlackScreen/Scripts/CursorManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackScreen
+{
+    public class CursorManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Cursor.visible = false;
+        }
+    }
+}
